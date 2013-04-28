d798ab29dc7de32ee60155035fb437278a5ea0a1
roger <roger@elvex.org.ar>
2013-04-28 19:18:32 -0300

    removed old indexer test

--- lucius/utils.py
+++ lucius/utils.py
@@ -13,13 +13,3 @@ class LuceneDocument(documents.document.Document):
         cls = field_types[field_type]
         field = cls(name, **params)
         self.add(next(field.items(*[value])))
-
-def index_doc(doc):
-    try:
-        data = doc["data"]
-        result = LuceneDocument()
-        result.add_field("song", data["song"], store=True)
-        result.add_field("artist", data["artist"], store=True)
-        return result
-    except KeyError:
-        pass
